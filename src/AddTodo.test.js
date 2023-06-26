b217f009ceba54e036cc80c888f516b8a0f1f7f4
liannnaa <lnp2016@nyu.edu>
2023-06-26 13:46:27 -0500

    tasks dont get added without name or due date

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -37,21 +37,37 @@ test('App component doesn\'t render duplicate Task', async () => {
   expect(items.length).toBe(1);
  });
 
- test('test that App component doesn\'t add a task without task name', () => {
+ test('App component doesn\'t add a task without task name', () => {
+  render(<App />);
+  const inputDate = screen.getByRole('textbox', {name: /Due Date/i});
+  const addButton = screen.getByRole('button', {name: /Add/i});
+
+  fireEvent.change(inputDate, { target: { value: '6/30/23'}});
+  fireEvent.click(addButton);
+
+  const noItemsText = screen.getByText("You have no todo's left");
+  expect(noItemsText).toBeInTheDocument();
+ });
+
+ test('App component doesn\'t add a task without due date', () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const addButton = screen.getByRole('button', {name: /Add/i});
+
+  fireEvent.change(inputTask, { target: { value: 'Test' } });
+  fireEvent.click(addButton);
+
+  const noItemsText = screen.getByText("You have no todo's left");
+  expect(noItemsText).toBeInTheDocument();
+ });
+
+
+
+ test('App component can be deleted thru checkbox', () => {
   render(<App />);
  });
 
- test('test that App component doesn\'t add a task without due date', () => {
-  render(<App />);
- });
 
-
-
- test('test that App component can be deleted thru checkbox', () => {
-  render(<App />);
- });
-
-
- test('test that App component renders different colors for past due events', () => {
+ test('App component renders different colors for past due events', () => {
   render(<App />);
  });
